5a833054b5dfc3e7b5552fd50c93b823d99aa571
Bhavesh-Mankar51 <bhaveshmankar770@gmail.com>
2025-08-07 16:03:52 +0530

    Update the requirements and setup files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name='health_assistant',
+    version='0.1.0',
+    author='Bhavesh Mankar',    
+    author_email="bhaveshmankar024@gmail.com",
+    packages=find_packages(),
+    install_requires=[]
+)
